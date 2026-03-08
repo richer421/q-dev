ee08c5accce4b2492760a554e2e75846abc4a800
richer <yxxchange@163.com>
2026-03-08 23:20:39 +0800

    fix: remove hardcoded Swagger annotations from main.go

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"q-dev/cmd"
+	"{{ .ModuleName }}/cmd"
 )
 
 //go:generate go run github.com/swaggo/swag/cmd/swag init -o ./gen/docs --parseDependency
 //go:generate go run ./gen/gorm_gen
 
-// @title       Q-Dev API
-// @version     1.0
-// @BasePath    /api
 func main() {
 	cmd.Execute()
 }
